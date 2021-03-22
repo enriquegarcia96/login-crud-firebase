b514476b24450f7beac17ccbc05d80760c73db9b
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-03-21 22:39:39 -0600

    Rutas protegidas y cerrar sesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
+import Admin from './components/Admin';
+import {auth} from './firebase';
+
 /*
  ====================================
     IMPORTO EL REACT ROUTER
@@ -9,11 +12,29 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
-  return (
+
+  const [firebaseUser, setFirebaseUser] = React.useState(false);
+
+  React.useEffect(() => {
+
+    // Obtiene el usuario con sesion activa */
+    auth.onAuthStateChanged(user => {
+      console.log(user);
+
+      if (user) {
+        setFirebaseUser(user);
+      }else{
+        setFirebaseUser(null);
+      }
+
+    })
+  }, [])
+
+  return firebaseUser !== false ? (
     <Router>
       <div className="container">
         
-        <Navbar />
+        <Navbar firebaseUser={firebaseUser}/>
 
       <Switch>
           <Route path="/" exact>
@@ -25,13 +46,15 @@ function App() {
           </Route>
 
           <Route path="/admin">
-            Administracion...
+            <Admin />
           </Route>
 
       </Switch>
       </div>
     </Router>
-  );
+  ) : (
+    <h1 className="text-center"> Cargando...</h1>
+  )
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { auth } from '../firebase.js';
+import { withRouter } from 'react-router';
+
+
 /*====================================
     IMPORTO EL REACT NAVBAR
  ===================================*/
 import { Link, NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ( props ) => {
+
+    /**========= FUNCIONES ========== */
+    const cerrarSesion = () =>{
+
+        //cierro la sesion activa
+        auth.signOut()
+            .then( () => {
+
+                //lo empujo al login
+                props.history.push('/login');
+            } )
+    }
+
+
     return (
         <div className="navbar navbar-dark bg-dark">
             <Link className="navbar-brand" to="/">AUTH</Link>
@@ -13,18 +31,36 @@ const Navbar = () => {
                     <NavLink className="btn btn-dark mr-2" to="/" exact>
                         Inicio
                     </NavLink>
-                    <NavLink className="btn btn-dark mr-2" to="/admin">
-                        Admin
-                    </NavLink>
-                    <NavLink className="btn btn-dark mr-2" to="/login">
-                        Login
-                    </NavLink>
+                    {
+                        props.firebaseUser !== null ? (
+
+                            <NavLink className="btn btn-dark mr-2" to="/admin">
+                                Admin
+                            </NavLink>
+
+                        ) :  null
+                    }
+
+                    {
+                        props.firebaseUser !== null ? (
+                            <button 
+                                className="btn btn-dark"
+                                onClick={ () => cerrarSesion()}
+                            >
+                                Cerrar Sesión
+                            </button>
+                        ) : (
+                            <NavLink className="btn btn-dark mr-2" to="/login">
+                                Login
+                            </NavLink>
+                        )
+                    }
                 </div>
             </div>
         </div>
     )
 }
 
-export default Navbar;
+export default withRouter (Navbar);
 
 
